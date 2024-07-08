c06d6e5971dacebf7ab157efdfb0fe4d7435f986
uaamacaspac <145097800+uaamacaspac@users.noreply.github.com>
2024-07-08 11:29:24 +0800

    Update Uriel.sol

--- Uriel.sol
+++ Uriel.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/access/Ownable.sol";
+
+contract Uriel is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Uriel", "UIL") {
+        _mint(msg.sender, initialSupply); // Mint initial supply to the deployer
+    }
+
+    // Function to mint tokens
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Function to burn tokens
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
